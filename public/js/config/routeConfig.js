8aa961bb593d52133f0ad79bf1edffdaf32b9764
Patrick Vieira Feitosa <31283687+patrickfeitosa@users.noreply.github.com>
2017-10-21 09:18:11 -0200

    Implementação de Interceptors para tratativa de pacotes httpProvider
    Implementação de Interceptors para tratativa de pacotes httpProvider

--- public/js/config/routeConfig.js
+++ public/js/config/routeConfig.js
@@ -32,6 +32,10 @@ angular.module("listaTelefonica").config(function($routeProvider){
 		}
 	});
 
+	$routeProvider.when("/error", {
+		templateUrl: "view/error.html"
+	});
+
 	$routeProvider.otherwise({ redirectTo:"/contatos"});
 });
 
